ab48fe42aae0c1fdf067ba97f76d8358fdbde3d1
blainegriffen <blaine_griffen@byu.edu>
2026-02-12 11:09:21 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "daily-kaizen-cache-v1";
+const CACHE_NAME = "daily-kaizen-cache-v2";
 const ASSETS = [
   "./",
   "./index.html",
@@ -26,3 +26,4 @@ self.addEventListener("fetch", (event) => {
     caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
+
